fd7b763af0bd7ff57618485c662efb2f67608f90
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-01-31 16:39:37 +0530

    commit

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
-
-    require("dotenv").config();
+require("dotenv").config();
 
 export default {
 
-    JWT_SECRET : process.env.JWT,
+    JWT_SECRET : process.env.JWT_SECRET,
 
     EMAIL: process.env.EMAIL, // testing email & password
 
     PASSWORD : process.env.PASSWORD,
 
-    ATLAS_URI: process.env.URL
+    ATLAS_URI: process.env.ATLAS_URI
 }
-
